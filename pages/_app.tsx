2d14cb5661b86d0ba77e797394a52e05e973d081
Tairan Liang <liangt21@hotmail.com>
2023-09-26 19:06:32 +1000

    deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
+// import { AppProps } from "next/app";
+
+// const App = ({ Component, pageProps }: AppProps) => {
+//   return <Component {...pageProps} />;
+// };
+
+// export default App;
+
+import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Component {...pageProps} />
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   );
 };
 
